9a1e47d1a2717b6f2159d20fa64f220e8ba4d761
mldion1 <matt@admiralinstruments.com>
2019-04-01 10:57:25 -0700

    special_compile--program skips data normalization step, reports the relative mag/phase of the two signals NOT normalized to the last point in the series

--- picoscopeTestApp2/picoscopeTestApp2_fixed_encoding.cpp
+++ picoscopeTestApp2/picoscopeTestApp2_fixed_encoding.cpp
@@ -6,21 +6,10 @@
 #include "NumberCruncher.h"
 #include "cal_experiment.h"
 
-//#define CONTROL_EXPERIMENT_200mV  //hook up all probes to the signal generator.
-//#define CONTROL_EXPERIMENT_50mV  //hook up all probes to the signal generator.
-//#define STAGE1_SETTING1_EXPERIMENT	//hook up probe 1 and signal generator to WE input, in series with 100Ohm resistor. Hook up probe 2 to TIA stage 1 output.
-//#define STAGE1_SETTING2_EXPERIMENT  //hook up probe 1 and signal generator to WE input, in series with 10kOhm resistor. Hook up probe 2 to TIA stage 1 output.
-//#define STAGE1_SETTING3_EXPERIMENT  //hook up probe 1 and signal generator to WE input, in series with 100kOhm resistor. Hook up probe 2 to TIA stage 1 output.
-#define STAGE2_SETTING1_EXPERIMENT  //hook up signal generator to WE input, in series with 100Ohm resistor. Hook up probe 1 to TIA stage 1 output, and hook up probe 2 to TIA stage 2 output.
-
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	/*cal_experiment exp;
-	exp.readExperimentParamsFile(argv[1], argv[2]);
-	exp.runExperiment();*/
-
 	if (argc < 3)
 		return 0;
 
@@ -34,7 +23,7 @@ int main(int argc, char *argv[])
 		data.insert(data.end(), exp.rawData.begin(), exp.rawData.end());
 	}
 
-	NumberCruncher::NormalizeMag(data);
+	//NumberCruncher::NormalizeMag(data);
 
 	/* write data */
 	std::ofstream fout;
